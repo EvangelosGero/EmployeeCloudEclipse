6aee137fc6597037eb199d8e0bf1fbfae58a86fe
Evangelos Gerogiannis <egdynamotors@gmail.com>
2017-01-22 13:06:30 +0200

    DynaShoppingCart_2.70 first Commit 22/1/2017

---
+++ Documents/NetBeansProjects/DynaShoppingCart_2.70/src/main/java/com/dynamotors/dynashoppingcart/entities/ItemsDeliveryAvailablePin.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dynamotors.dynashoppingcart.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author e
+ */
+@Entity
+@Table(name = "items_delivery_available_pin", catalog = "dynashoppingcart", schema = "")
+@XmlRootElement
+
+public class ItemsDeliveryAvailablePin implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Size(max = 10)
+    @Column(name = "item_code", length = 10)
+    private String itemCode;
+    @Size(max = 10)
+    @Column(name = "pin", length = 10)
+    private String pin;
+
+    public ItemsDeliveryAvailablePin() {
+    }
+
+    public ItemsDeliveryAvailablePin(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    @Override
+    public int hashCode() {
+        Integer hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ItemsDeliveryAvailablePin)) {
+            return false;
+        }
+        ItemsDeliveryAvailablePin other = (ItemsDeliveryAvailablePin) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.dynamotors.dynashoppingcart.entities.ItemsDeliveryAvailablePin[ id=" + id + " ]";
+    }
+    
+}
