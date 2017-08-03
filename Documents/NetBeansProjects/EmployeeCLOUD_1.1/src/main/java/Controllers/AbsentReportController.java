21e91ecb393c4426e1fbae3e48535e835dd11bb8
Evangelos Gerogiannis <egdynamotors@gmail.com>
2017-08-03 15:30:38 +0300

    AbsentReport todo...

---
+++ Documents/NetBeansProjects/EmployeeCLOUD_1.1/src/main/java/Controllers/AbsentReportController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Controllers.Logic.AbsentReportPOJO;
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author evgero
+ */
+@Named("absentReportController")
+@SessionScoped
+public class AbsentReportController implements Serializable{
+    
+    private List<AbsentReportPOJO> data = new ArrayList<>();
+    private PreparedStatement prStm;
+    private Statement workersStm ;
+    private ResultSet workersRs, rs; 
+    LocalDate startDate;
+    LocalDate endDate;
+    LocalDate hireDate, apolisiDate;
+    String justify = "ΑΔΙΚΑΙΟΛΟΓΗΤΗ";
+
+    public List<AbsentReportPOJO> getData() {
+        return data;
+    }
+
+    public void setData(List<AbsentReportPOJO> data) {
+        this.data = data;
+    }
+    
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    
+}
---
+++ Documents/NetBeansProjects/EmployeeCLOUD_1.1/src/main/java/Controllers/Logic/AbsentReportPOJO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers.Logic;
+
+import java.util.Date;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author Home
+ */
+public class AbsentReportPOJO {
+    
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Date absentDate;
+    private String justify;
+
+    public AbsentReportPOJO() {
+    }
+        
+    public AbsentReportPOJO(Integer id, String firstName, String lastName, Date absentDate, String justify) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.absentDate = absentDate;
+        this.justify = justify;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getAbsentDate() {
+        return absentDate;
+    }
+
+    public void setAbsentDate(Date absentDate) {
+        this.absentDate = absentDate;
+    }
+
+    public String getJustify() {
+        return justify;
+    }
+
+    public void setJustify(String justify) {
+        this.justify = justify;
+    }
+    
+    
+}   
+            
+        
+
+
+    
+
