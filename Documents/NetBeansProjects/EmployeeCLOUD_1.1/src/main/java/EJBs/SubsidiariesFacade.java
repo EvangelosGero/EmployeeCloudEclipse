a0c720b2695f615b48fce915628298adc2b5d17a
Evangelos Gerogiannis <egdynamotors@gmail.com>
2017-08-21 13:38:31 +0300

    CreateVacationReport to be checked with the popStage. Otherwise history  menu completed. company_information, subsidiaries OK

---
+++ Documents/NetBeansProjects/EmployeeCLOUD_1.1/src/main/java/EJBs/SubsidiariesFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EJBs;
+
+import Entities.Subsidiaries;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author evgero
+ */
+@Stateless
+public class SubsidiariesFacade extends AbstractFacade<Subsidiaries> {
+
+    @PersistenceContext(unitName = "com.dynamotors_Timer1.5_REST_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SubsidiariesFacade() {
+        super(Subsidiaries.class);
+    }
+    
+}
---
+++ Documents/NetBeansProjects/EmployeeCLOUD_1.1/src/main/java/Entities/Subsidiaries.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author evgero
+ */
+@Entity
+@Table(name = "SUBSIDIARIES", catalog = "", schema = "APP")
+@XmlRootElement
+public class Subsidiaries implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID", nullable = false)
+    private Short id;
+    @Size(max = 40)
+    @Column(name = "NAME", length = 40)
+    private String name;
+
+    public Subsidiaries() {
+    }
+
+    public Subsidiaries(Short id) {
+        this.id = id;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Subsidiaries)) {
+            return false;
+        }
+        Subsidiaries other = (Subsidiaries) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entities.Subsidiaries[ id=" + id + " ]";
+    }
+    
+}
