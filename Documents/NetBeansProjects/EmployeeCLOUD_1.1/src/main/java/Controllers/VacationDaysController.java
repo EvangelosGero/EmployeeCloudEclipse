f6f331994b4ec6841305b7113b21def52b6b667d
Evangelos Gerogiannis <egdynamotors@gmail.com>
2017-08-21 17:03:48 +0300

    CreateVacationReport to be checked with the popStage OK . PDF and Excel remaining

--- Documents/NetBeansProjects/EmployeeCLOUD_1.1/src/main/java/Controllers/VacationDaysController.java
+++ Documents/NetBeansProjects/EmployeeCLOUD_1.1/src/main/java/Controllers/VacationDaysController.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import Controllers.Logic.CreateVacationReport;
 import Controllers.Logic.showVacationReport;
+import Controllers.util.DropIfExists;
 import Entities.VacationDays;
 import Controllers.util.JsfUtil;
 import Controllers.util.JsfUtil.PersistAction;
@@ -9,12 +10,17 @@ import EJBs.VacationDaysFacade;
 import com.dynamotors.timer1._rest.Workers;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +29,7 @@ import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -128,7 +135,7 @@ public class VacationDaysController implements Serializable {
     }
     
     public void createVacationReport() throws SQLException{
-        if ( LocalDate.now().getMonth() == Month.JANUARY){
+        if ( LocalDate.now().getMonth() == Month.AUGUST){
             RequestContext context = RequestContext.getCurrentInstance();        
             context.execute("PF('vacationDlgWV').show();");        
             context.update("vacationGT2:GT2Box");
@@ -137,18 +144,136 @@ public class VacationDaysController implements Serializable {
     }
     
     public void metaforaVacation(){
+        PreparedStatement prStm = null;
+        Map<Integer, Integer> consumedMap = new HashMap<>();
+        List<LocalDate> holidaysList  = new ArrayList<>();
         int currentYear = LocalDate.now().getYear();
+        int previousMonth = LocalDate.now().minusMonths(1).getMonthValue();
+        int tableYear = LocalDate.now().minusMonths(1).getYear();
+        Connection con = this.emplAdminsController.getCon();
         Statement stm = null;
         Statement ssss = null;
         ResultSet rsss = null;
         try {
             if (this.isVacationRadio()){
-                new CreateVacationReport().createLastYearDayReport(currentYear-1);
-                String query2 = "SELECT id, remaining_days FROM VACATION_REPORT_" +Integer.toString(currentYear-1);
-                ssss = emplAdminsController.getCon().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                                              
+            String tableStr = "REPORT_"+Integer.toString(previousMonth)
+                    +"_"+Integer.toString(tableYear);
+            
+            
+            DropIfExists.exec(con, "VIEW", "TEMP2");
+            DropIfExists.exec(con, "VIEW", "TEMP1");
+            DropIfExists.exec(con, "TABLE", "TEMP");
+            
+             /*TEMP1 is used to add any new employees in the list and remove the fired of last year*/ 
+            
+            
+            String query = "CREATE VIEW TEMP1 (ID, firstname, lastname) AS "                   
+                    + "SELECT ID, first_name, last_name FROM workers "                    
+                    + "WHERE (workers.apolisi = 0 OR (workers.apolisi <> 0 "
+                    + "AND (YEAR(workers.diakopi) = " +Integer.toString(tableYear)+ ")"
+                    + "AND (MONTH(workers.diakopi) > " +Integer.toString(previousMonth - 1)+ ")))";
+            
+            stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);                      
+            int update = stm.executeUpdate(query);
+            if (stm != null){stm.close();}
+            
+            
+            
+            query = "CREATE TABLE TEMP ( " +
+                            "ID smallint, " +
+                            "firstname varchar(20), " +
+                            "lastname varchar(20), " +
+                            "ENTITLED_DAYS smallint DEFAULT 0, " +
+                            "LASTYEAR_DAYS smallint DEFAULT 0, " +
+                            "CONSUMED_DAYS smallint DEFAULT 0, " +
+                            "REMAINING_DAYS smallint DEFAULT 0)";            
+            
+            stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);                      
+            update = stm.executeUpdate(query);
+            if (stm!= null){stm.close();} 
+            
+            
+            
+            query = "CREATE VIEW TEMP2 (ID, firstname, lastname, entitled_days, lastyear_days, " +
+                    "consumed_days, remaining_days) AS " +
+            "SELECT TEMP1.ID, TEMP1.firstname, TEMP1.lastname, T1.entitled_days, " +
+            "T1.lastyear_days, T1.consumed_days, " +
+            "T1.remaining_days " +
+            "FROM TEMP1 " +
+            "LEFT JOIN (EMPLOYEE_VACATION "  
+                    + " AS T1) "
+    //        + " JOIN workers "
+    //        + "ON T1.id = workers.id "
+    //                + "AND (workers.apolisi = 0 OR (workers.apolisi <> 0 "
+    //                + "AND (YEAR(workers.diakopi) = " +Integer.toString(tableYear)+ "))) "                            
+            +"ON TEMP1.ID = T1.ID ";
+            
+            
+            
+            stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);                      
+            update = stm.executeUpdate(query);
+            if (stm != null){stm.close();}
+            
+             /*Compute (IN JAVA !!) the consumed days from VACATION_DAYS table*/        
+                
+        stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        String sql1 = "SELECT * FROM holidays";
+        ResultSet rs1 = stm.executeQuery(sql1);
+        /* Put rs1 in a List */        
+        while (rs1.next()){            
+            holidaysList.add(rs1.getDate(1).toLocalDate());
+        }
+        if (stm != null) { stm.close();}
+        if (rs1 != null) {rs1.close();}
+        
+        new CreateVacationReport(emplAdminsController.getCon()).computeConsumedDays(currentYear) ;       
+                       
+        query = "INSERT INTO TEMP (ID, firstname, lastname, entitled_days, lastyear_days " +
+                ", consumed_days, remaining_days " +
+                ") SELECT t.ID, t.firstname, t.lastname, t.ENTITLED_DAYS, t.LASTYEAR_DAYS " +
+                ", 0, 0 " +    
+                "FROM TEMP2 t " ;
+        
+        stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);                      
+        update = stm.executeUpdate(query);
+        if (stm != null){stm.close();}
+    
+         /* Set to Zero any null values in TEMP */ 
+        
+        query = "UPDATE temp set lastyear_days = 0 where lastyear_days IS NULL";
+        stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+        update = stm.executeUpdate(query);
+        if (stm != null){stm.close();}                        
+        query = "UPDATE temp set consumed_days = 0 where consumed_days IS NULL";
+        stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+        update = stm.executeUpdate(query);
+        if (stm != null){stm.close();}
+    
+        /*Now update the consumed days in TEMP*/
+    
+        for(Map.Entry<Integer, Integer> entry : consumedMap.entrySet())
+        {        
+            String str = "UPDATE temp SET consumed_days = ? WHERE id = ? ";        
+            prStm = con.prepareStatement(str);    
+            prStm.setInt(1, entry.getValue());
+            prStm.setInt(2, entry.getKey());
+            int updatedRows = prStm.executeUpdate();    
+            if (prStm != null) {prStm.close();}
+        }
+        new CreateVacationReport(emplAdminsController.getCon()).createLastYearDayReport(currentYear-1);
+                
+        String query2 = "SELECT id, remaining_days FROM VACATION_REPORT_" +Integer.toString(currentYear-1);
+        ssss = emplAdminsController.getCon().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
                         ResultSet.CONCUR_UPDATABLE);
-                rsss = ssss.executeQuery(query2);
-                while (rsss.next()){
+        rsss = ssss.executeQuery(query2);
+        while (rsss.next()){
                     String query1 = "UPDATE temp SET lastyear_days = "+Integer.toString(rsss.getInt("remaining_days"))+
                             " WHERE temp.id = "+Integer.toString(rsss.getInt("id"));
                     stm = emplAdminsController.getCon().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
@@ -156,14 +281,54 @@ public class VacationDaysController implements Serializable {
                     int update1 = stm.executeUpdate(query1);
                     if(stm != null)stm.close();
                 
-                }
             }
-            new CreateVacationReport().CreateVacationDBTable(emplAdminsController.getCon());
+         /* Now compute the remaining days correctly */
+    
+        query = "UPDATE temp SET remaining_days = entitled_days + lastyear_days - consumed_days ";                            
+                           
+        stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                            ResultSet.CONCUR_UPDATABLE);
+        update = stm.executeUpdate(query); 
+    
+            JsfUtil.addSuccessMessage("Η δημιουργία του report αδειών ολοκληρώθηκε!"); 
+        
+          }
+            
         } catch (SQLException ex) {
             Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
             JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
         }finally {
-                {try {
+                try {
+                    
+                    String query = "DROP VIEW TEMP2" ;
+        
+                stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                   ResultSet.CONCUR_UPDATABLE);                      
+                int update = stm.executeUpdate(query);
+                if (stm != null){stm.close();}
+        
+                query = "DROP VIEW TEMP1" ;
+        
+                stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);                      
+                update = stm.executeUpdate(query);
+                if (stm!= null) {stm.close();}
+        
+                query = "DROP TABLE EMPLOYEE_VACATION" ;
+        
+                stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);                      
+                update = stm.executeUpdate(query);
+                if (stm != null){stm.close();}
+
+                query = "RENAME TABLE TEMP TO EMPLOYEE_VACATION";
+
+                stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+                      
+                update = stm.executeUpdate(query);
+                if (stm != null){stm.close();}
+                    
                     if (stm != null)stm.close();
                     if (rsss != null)rsss.close();
                     if (ssss != null)ssss.close();
@@ -171,8 +336,8 @@ public class VacationDaysController implements Serializable {
                     Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
                     JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
                 }
-                }
-          }
+                
+            }
     }
     
     
